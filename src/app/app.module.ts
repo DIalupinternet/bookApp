351dc15e859654ed45418a4f5d0e29e8338ea599
CN90 <chr.revel@gmail.com>
2017-10-30 20:44:05 -0700

    Added navbar & sidebar component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    SidebarComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule
